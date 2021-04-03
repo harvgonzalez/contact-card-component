9efcb34c68bb62303dcb6dac140e5e200f5c4a98
Javier Gonzalez <jgromeo93@gmail.com>
2021-04-03 21:34:40 +0200

    app takes random users from an api call with reusable component card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import ContactCard from './ContactCard';
+const App = () =>{
+  const contacts  = [
+    {
+      avatarUrl: "https://via.placeholder.com/150", 
+      name: "Jenny Han",
+      email: "jenny@notreal.com",
+      age: 21
+    },
+    {
+      avatarUrl: "https://via.placeholder.com/150", 
+      name: "Peter Pan" ,
+      email: "peter@noreal.es",
+      age: 27
+    },
+    {
+      avatarUrl: "https://via.placeholder.com/150", 
+      name: "Tony Start" ,
+      email: "tony@marvel.com" ,
+      age: 70
+    },
+    {
+      avatarUrl: "https://via.placeholder.com/150", 
+      name: "Lisa Simpson" ,
+      email: "lisa@springfield.com" ,
+      age: 12
+    }
+  ];
+  const [results, setResults] = useState([]);
+  useEffect(()=>{
+    fetch("https://randomuser.me/api/?results=4")
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      setResults(data.results);
+     });
+  }, [])
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {results.map((result, index)=>{
+        return(
+          <ContactCard key={index}
+           avatarUrl = {result.picture.large}
+           name = {result.name.first}
+           email = {result.email}
+           age = {result.dob.age} />
+        )
+      })}
     </div>
   );
 }
